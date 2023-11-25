a96ae17de05fb38d780adccb91237f5dd3ee9686
hiimhoanglam <110579854+hiimhoanglam@users.noreply.github.com>
2023-11-25 11:07:23 +0700

    testing merge

--- src/Chess.java
+++ src/Chess.java
@@ -2,5 +2,8 @@ public class Chess {
     public static void main(String[] args) {
         System.out.println("This is chess");
         System.out.println("This is a new line");
+        System.out.println("This is a line from a branch");
+        System.out.println("This is also a new line from a branch");
+        System.out.println("Testing merge");
     }
 }
